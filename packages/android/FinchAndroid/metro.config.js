c5a4fb6646b2f20de1835ee6191f281c4c782a20
Kyle Christian <kyle.christian@gmail.com>
2025-10-14 06:15:57 -0600

    fully working android

--- packages/android/FinchAndroid/metro.config.js
+++ packages/android/FinchAndroid/metro.config.js
@@ -1,13 +1,34 @@
 const path = require('path');
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
 const config = {
-  watchFolders: [path.resolve(__dirname, '../../..')],
+  // 1. WATCH FOLDERS
+  watchFolders: [
+    // The root of the monorepo
+    path.resolve(__dirname, '../../..'),
+    // The shared-logic package (Corrected Path)
+    path.resolve(__dirname, '../../shared-logic'),
+  ],
+
+  // 2. EXTRA NODE MODULES
   resolver: {
-    nodeModulesPaths: [
-      path.resolve(__dirname, 'node_modules'),
-      path.resolve(__dirname, '../../../node_modules'),
-    ],
+    extraNodeModules: new Proxy(
+      {},
+      {
+        get: (target, name) => {
+          if (name === 'react' || name === 'react-native') {
+            return path.join(__dirname, `node_modules/${name}`);
+          }
+          return path.join(process.cwd(), `node_modules/${name}`);
+        },
+      },
+    ),
   },
 };
 
