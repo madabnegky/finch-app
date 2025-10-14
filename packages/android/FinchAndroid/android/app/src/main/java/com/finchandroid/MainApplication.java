9161db56268cf1a0a69a72c53a76ce4f58161154
Kyle Christian <kyle.christian@gmail.com>
2025-10-14 05:43:26 -0600

    clean up, clean config

--- packages/android/FinchAndroid/android/app/src/main/java/com/finchandroid/MainApplication.java
+++ packages/android/FinchAndroid/android/app/src/main/java/com/finchandroid/MainApplication.java
@@ -1,6 +1,7 @@
 package com.finchandroid;
 
 import android.app.Application;
+import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -8,8 +9,6 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
-// We are removing the import for the missing PackageList
-// import com.facebook.react.PackageList;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -22,11 +21,10 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected List<ReactPackage> getPackages() {
-       // This is the correct way to manually create the list of core packages.
-       List<ReactPackage> packages = new java.util.ArrayList<>();
-       packages.add(new com.facebook.react.shell.MainReactPackage());
-        return packages;
-}
+          @SuppressWarnings("UnnecessaryLocalVariable")
+          List<ReactPackage> packages = new PackageList(this).getPackages();
+          return packages;
+        }
 
         @Override
         protected String getJSMainModuleName() {
@@ -54,9 +52,9 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
       DefaultNewArchitectureEntryPoint.load();
     }
-    ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+    // Flipper is disabled.
+    // ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
 }
